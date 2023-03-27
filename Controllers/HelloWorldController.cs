2534fb0238b005dec856fb4bc3465bdfae3f617a
chrishiggi <chrishiggi@gmail.com>
2023-03-27 16:52:57 +0000

    add helloworld and welcome views

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,16 +5,18 @@ namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-  // 
-  // GET: /HelloWorld/
-  public IActionResult Index()
-  {
-    return View();
-  }
-  // GET: /HelloWorld/Welcome/ 
-  // Requires using System.Text.Encodings.Web;
-  public string Welcome(string name, int numTimes = 1)
-  {
-    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-  }
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
 }
